fec7677dba2070bf804fe77ccac4916457a420ee
jonsca <jonathans1978@hotmail.com>
2014-11-19 13:46:16 -0500

    Commit of the TwitterAPItest code

---
+++ TwitterAPItest/TwitterAPItest/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TwitterAPItest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tweetBox = new System.Windows.Forms.TextBox();
+            this.avatarBox = new System.Windows.Forms.PictureBox();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.pinBox = new System.Windows.Forms.TextBox();
+            this.pinBtn = new System.Windows.Forms.Button();
+            this.getMoreBtn = new System.Windows.Forms.Button();
+            this.rowsAddedTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.avatarBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // tweetBox
+            // 
+            this.tweetBox.Location = new System.Drawing.Point(296, 62);
+            this.tweetBox.Multiline = true;
+            this.tweetBox.Name = "tweetBox";
+            this.tweetBox.Size = new System.Drawing.Size(279, 62);
+            this.tweetBox.TabIndex = 1;
+            // 
+            // avatarBox
+            // 
+            this.avatarBox.Location = new System.Drawing.Point(268, 8);
+            this.avatarBox.Name = "avatarBox";
+            this.avatarBox.Size = new System.Drawing.Size(79, 50);
+            this.avatarBox.TabIndex = 2;
+            this.avatarBox.TabStop = false;
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(354, 8);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(193, 20);
+            this.usernameBox.TabIndex = 3;
+            // 
+            // pinBox
+            // 
+            this.pinBox.Location = new System.Drawing.Point(269, 230);
+            this.pinBox.Name = "pinBox";
+            this.pinBox.Size = new System.Drawing.Size(150, 20);
+            this.pinBox.TabIndex = 5;
+            // 
+            // pinBtn
+            // 
+            this.pinBtn.Location = new System.Drawing.Point(423, 227);
+            this.pinBtn.Name = "pinBtn";
+            this.pinBtn.Size = new System.Drawing.Size(75, 23);
+            this.pinBtn.TabIndex = 6;
+            this.pinBtn.Text = "Enter Pin";
+            this.pinBtn.UseVisualStyleBackColor = true;
+            this.pinBtn.Click += new System.EventHandler(this.pinBtn_Click);
+            // 
+            // getMoreBtn
+            // 
+            this.getMoreBtn.Location = new System.Drawing.Point(377, 144);
+            this.getMoreBtn.Name = "getMoreBtn";
+            this.getMoreBtn.Size = new System.Drawing.Size(132, 23);
+            this.getMoreBtn.TabIndex = 7;
+            this.getMoreBtn.Text = "Grab Some More";
+            this.getMoreBtn.UseVisualStyleBackColor = true;
+            this.getMoreBtn.Click += new System.EventHandler(this.getMoreBtn_Click);
+            // 
+            // rowsAddedTB
+            // 
+            this.rowsAddedTB.Location = new System.Drawing.Point(505, 227);
+            this.rowsAddedTB.Name = "rowsAddedTB";
+            this.rowsAddedTB.ReadOnly = true;
+            this.rowsAddedTB.Size = new System.Drawing.Size(82, 20);
+            this.rowsAddedTB.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(554, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "RT";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rowsAddedTB);
+            this.Controls.Add(this.getMoreBtn);
+            this.Controls.Add(this.pinBtn);
+            this.Controls.Add(this.pinBox);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.avatarBox);
+            this.Controls.Add(this.tweetBox);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.avatarBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox tweetBox;
+        private System.Windows.Forms.PictureBox avatarBox;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox pinBox;
+        private System.Windows.Forms.Button pinBtn;
+        private System.Windows.Forms.Button getMoreBtn;
+        private System.Windows.Forms.TextBox rowsAddedTB;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TwitterAPItest/TwitterAPItest/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Web;
+using Twitterizer;
+using Newtonsoft;
+using System.Xml;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace TwitterAPItest
+{
+    public partial class Form1 : Form
+    {
+        private OAuthTokenResponse oatr;
+        private Uri uri;
+        private OAuthTokenResponse oatrat;
+        private MySqlConnection myconn;
+
+        private string consumerkey = "1234";  //redacted for public view
+        private string consumersecret = "1234";  //redacted
+
+        private OAuthTokens oats;
+
+        private decimal ? lastID = null;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            //mark my spot in the tweetstream in the database for between sessions
+            webBrowser1.Visible = false;
+            pinBox.Visible = false;
+            pinBtn.Visible = false;
+                
+            myconn = new MySqlConnection("server=localhost;user=root;database=twitterdb;port=3306");
+            myconn.Open();
+            
+            MySqlCommand checkauth = myconn.CreateCommand();
+            checkauth.CommandText = "SELECT AccessToken,AccessTokenSecret FROM OAuthINFO WHERE idOAuthInfo = 1";
+            MySqlDataReader sqldata = checkauth.ExecuteReader();
+            sqldata.Read();
+            if (sqldata[0] is System.DBNull || sqldata[1] is System.DBNull)
+            {
+                authorize();
+            }
+            else
+            {
+                oats = new OAuthTokens()
+                {
+                    AccessToken = (string) sqldata[0],
+                    AccessTokenSecret = (string) sqldata[1],
+                    ConsumerKey = consumerkey,
+                    ConsumerSecret = consumersecret
+                };
+
+            }
+
+            //Need to now be able to persist the last ID to bring that back.
+
+            sqldata.Close();
+            
+        }        
+
+        private void pinBtn_Click(object sender, EventArgs e)
+        {
+            
+            
+            oatrat = OAuthUtility.GetAccessToken(consumerkey, consumersecret, oatr.Token, pinBox.Text);
+            oats = new OAuthTokens()
+                                    {
+                                        AccessToken = oatrat.Token,
+                                        AccessTokenSecret = oatrat.TokenSecret,
+                                        ConsumerKey = consumerkey,
+                                        ConsumerSecret = consumersecret
+                                    };
+
+            MySqlCommand updateaccess = myconn.CreateCommand();
+            updateaccess.CommandText = "UPDATE OAuthInfo SET AccessToken = @accesstoken, AccessTokenSecret = @accesstokensecret WHERE idOAuthInfo=1";
+            updateaccess.Parameters.Add("@accesstoken",MySqlDbType.VarChar,100);
+            updateaccess.Parameters.Add("@accesstokensecret",MySqlDbType.VarChar,45);
+            updateaccess.Parameters["@accesstoken"].Value = oatrat.Token;
+            updateaccess.Parameters["@accesstokensecret"].Value = oatrat.TokenSecret;
+
+            updateaccess.ExecuteNonQuery();
+
+            webBrowser1.Visible = false;
+            pinBox.Visible = false;
+            pinBtn.Visible = false;
+                   
+        }
+        
+        private void authorize()
+        {
+            webBrowser1.Visible = true;
+            pinBox.Visible = true;
+            pinBtn.Visible = true;
+            pinBox.Text = "Enter the pin here";
+            oatr = OAuthUtility.GetRequestToken(consumerkey, consumersecret, "oob");
+            uri = OAuthUtility.BuildAuthorizationUri(oatr.Token);
+            webBrowser1.Navigate(uri.ToString());
+        
+        }
+
+        private void getMoreBtn_Click(object sender, EventArgs e)
+        {                        
+            TimelineOptions to = new TimelineOptions();
+            
+            if (lastID != null)
+            {
+                to.SinceStatusId = (decimal)lastID;
+            }
+            
+            TwitterResponse<TwitterStatusCollection> homeTimeline = TwitterTimeline.HomeTimeline(oats,to);
+
+            List<TwitterStatus> tsl = homeTimeline.ResponseObject.ToList<TwitterStatus>();
+            if (tsl.Count > 0)
+            {   //[0] is the last tweet to come in
+                avatarBox.ImageLocation = tsl[0].User.ProfileImageLocation;
+                avatarBox.SizeMode = PictureBoxSizeMode.AutoSize;
+                usernameBox.Text = tsl[0].User.Name;
+                tweetBox.Text = tsl[0].Text;
+
+                if (tsl[0].Retweeted)
+                    label1.Visible = true;
+                else
+                    label1.Visible = false;
+
+                lastID = tsl.First().Id;
+                
+                tsl.Reverse();               
+                
+                addToDB(tsl);
+
+                
+            }
+            
+            
+
+            rowsAddedTB.Text = tsl.Count + " Rows Added";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myconn.Close();
+            //get ID from last post here
+        }
+
+        private void addToDB(List<TwitterStatus> twitterstatuslist)
+        {
+             MySqlCommand mySqlCommand = myconn.CreateCommand();
+            mySqlCommand.CommandText =
+                "INSERT INTO Tweets (" +
+                " userName, userAvatar, tweetText,datetime" +
+                ") VALUES (" +
+                " @userName,@userAvatar, @tweetText, @datetime " +
+                ")";
+
+            mySqlCommand.Parameters.Add("@userName", MySqlDbType.VarChar, 45);
+            mySqlCommand.Parameters.Add("@userAvatar", MySqlDbType.VarChar, 256);
+            mySqlCommand.Parameters.Add("@tweetText",MySqlDbType.VarChar,140);
+            mySqlCommand.Parameters.Add("@datetime", MySqlDbType.DateTime);
+
+            foreach (TwitterStatus ts in twitterstatuslist)
+            {
+
+                mySqlCommand.Parameters["@userName"].Value = ts.User.Name;
+                mySqlCommand.Parameters["@userAvatar"].Value = ts.User.ProfileImageLocation;
+                mySqlCommand.Parameters["@tweetText"].Value = ts.Text;
+                mySqlCommand.Parameters["@datetime"].Value = ts.CreatedDate.ToString();
+
+                mySqlCommand.ExecuteNonQuery();
+            }
+
+            rowsAddedTB.Text = twitterstatuslist.Count + " Rows Added";
+        }
+        
+    }
+}
